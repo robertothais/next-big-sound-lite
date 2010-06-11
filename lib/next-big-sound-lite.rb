dbd9ea24d8ab59162512b94f38b054dda3817ae7
Roberto Thais <roberto.n.thais@gmail.com>
2010-06-11 17:07:45 -0400

    added write endpoints, refactored classes into files, fixed bug with setting key

--- lib/next-big-sound-lite.rb
+++ lib/next-big-sound-lite.rb
@@ -1,116 +1,33 @@
+#For version 2.0 of the Next Big Sound API
+
 require 'rest_client'
 require 'json'
 
-#for version 2.0 of the NBS API
+require 'next-big-sound-lite/resource'
+require 'next-big-sound-lite/artist'
+require 'next-big-sound-lite/genre'
+require 'next-big-sound-lite/metric'
+require 'next-big-sound-lite/profile'
+require 'next-big-sound-lite/service'
+require 'next-big-sound-lite/track'
+require 'next-big-sound-lite/youtube'
+
 module NBS
+  
+  class << self
+    attr_reader :base
+    attr_reader :private_key
+  end
   
   def self.api_key=(key)
     @base = RestClient::Resource.new("http://#{key}.api2.nextbigsound.com")
   end  
   
-  def self.base
-    @base
+  def self.private_key=(key)
+    @private_key = key
   end
   
   self.api_key = 'key'
-  
-  class Artist
-    
-    @resource = NBS.base['artists']
-    
-    def self.view(id)
-      res = @resource["view/#{id}.json"].get
-      JSON.parse(res)
-    end
-    
-    def self.search(q)
-      res = @resource["search.json?q=#{CGI.escape(q)}"].get
-      JSON.parse(res)
-    end
-    
-    def self.ranking(type, ids)
-      ids = ids.join('-')
-      res = @resource["rank/#{type}/#{ids}.json"].get
-      JSON.parse(res)
-    end  
-    
-  end
-  
-  class Genre
-    
-    @resource = NBS.base['genres']
-    
-    def self.artist(id)
-      res = @resource["artist/#{id}.json"].get
-      JSON.parse(res)
-    end
-  end
-  
-  class Metric
-    
-    @resource = NBS.base['metrics']
-    
-    def self.profile(id)
-      res = @resource["profile/#{id}.json"].get
-      JSON.parse(res)
-    end
-    
-    def self.artist(id)
-      res = @resource["artist/#{id}.json"].get
-      JSON.parse(res)
-    end
-  end
-  
-  class Profile
-    
-    @resource = NBS.base['profiles']
-    
-    def self.artist(id)
-      res = @resource["artist/#{id}.json"].get
-      JSON.parse(res)
-    end
-    
-    def self.search(url)
-      res = @resource["search.json?u=#{CGI.escape(url)}"].get
-      JSON.parse(res)
-    end
-  end
-  
-  class Service
-    
-    @resource = NBS.base['services']
-    
-    def self.list
-      JSON.parse(@resource['.json'].get)
-    end
-    
-  end
-  
-  class Track
-    
-    @resource = NBS.base['tracks']
-    
-    def self.artist(id)
-      res = @resource["artist/#{id}.json"].get
-      JSON.parse(res)
-    end
-    
-    def self.profile(id)
-      res = @resource["profile/#{id}.json"].get
-      JSON.parse(res)
-    end
-    
-  end
-  
-  class Youtube
-    
-    @resource = NBS.base['youtube']
-    
-    def self.artist(id)
-      res = @resource["artist/#{id}.json"].get
-      JSON.parse(res)
-    end
-    
-  end
-  
+
 end
+
